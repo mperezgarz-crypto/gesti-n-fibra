d2f846563253b42cffbe0a8832523ea4ee58e69d
Mikel <mperezgarz@educacion.navarra.es>
2026-05-17 13:01:46 +0200

    Ruta sqlite render

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,17 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./database/fibra.db');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
+const dbPath = path.join(__dirname, '../database/fibra.db');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error("Error al conectar con SQLite", err);
+    } else {
+        console.log("Conectado a SQLite");
+    }
+});
 db.serialize(() => {
     db.run(`
         CREATE TABLE IF NOT EXISTS intervenciones (
