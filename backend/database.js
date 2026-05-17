ddf20d035d93627d0a22152347cc2e08bcfdb3d1
Mikel <mperezgarz@educacion.navarra.es>
2026-05-17 13:10:31 +0200

    Fix sqlite

--- backend/database.js
+++ backend/database.js
@@ -1,18 +1,19 @@
 const sqlite3 = require('sqlite3').verbose();
-
-const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const dbPath = path.join(__dirname, '../database/fibra.db');
 
 const db = new sqlite3.Database(dbPath, (err) => {
+
     if (err) {
         console.error("Error al conectar con SQLite", err);
     } else {
         console.log("Conectado a SQLite");
     }
 });
+
 db.serialize(() => {
+
     db.run(`
         CREATE TABLE IF NOT EXISTS intervenciones (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,7 +24,6 @@ db.serialize(() => {
             cto TEXT,
             cableado TEXT,
             fecha TEXT
-            fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     `);
 });
